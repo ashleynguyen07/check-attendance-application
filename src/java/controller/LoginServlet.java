fdee82472ac81c39753095bba682dfbe0e5e9a0a
ashleynguyen07 <nguyentramy.se@gmail.com>
2023-06-21 13:16:36 +0700

    login dashboard

--- src/java/controller/LoginServlet.java
+++ src/java/controller/LoginServlet.java
@@ -4,11 +4,9 @@
  */
 package controller;
 
-import jakarta.servlet.ServletException;
+import database.LoginDAO;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -17,19 +15,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author tramy
  */
-@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+@WebServlet(name = "loginServlet", urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
 
     /**
@@ -41,61 +32,77 @@ public class LoginServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    private static final long serialVersionUID = 1L;
-
-    private String jdbcURL;
-    private String jdbcUsername;
-    private String jdbcPassword;
-
-    public void init() {
-        jdbcURL = "jdbc:sqlserver://localhost:1433;database=EmployeeManagement;encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-        jdbcUsername = "sa";
-        jdbcPassword = "someThingComplicated1234";
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        final String tenDangNhap = request.getParameter("username");
+        String matKhau = request.getParameter("password");
 
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            conn = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
-
-            String sql = "SELECT * FROM LOGIN WHERE Username = ? AND Password = ?";
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, username);
-            pstmt.setString(2, password);
-            rs = pstmt.executeQuery();
-
-            if (rs.next()) { //luu role vao session
-                int role = rs.getInt("Role");
-                HttpSession session = request.getSession();
-                session.setAttribute("username", username);
-                session.setAttribute("role", role);
-
-                if (role == 1) {
-                    response.sendRedirect("admin-dashboard.jsp");
-                } else if (role == 2) {
-                    response.sendRedirect("user-dashboard.jsp");
-                }
-            } else {
-                request.setAttribute("errorMessage", "Invalid username or password");
-                request.getRequestDispatcher("front-end/user-login.jsp");
-            }
+        final model.LoginModel tka = new model.LoginModel();
+        tka.setUsername(tenDangNhap);
+        tka.setPassword(matKhau);
+        final LoginDAO tkad = new LoginDAO();
+        final model.LoginModel taiKhoan = tkad.selectByUserAndPassword(tka);
+        String url = "";
+        if (taiKhoan == null) {
+            request.setAttribute("baoLoi", (Object) "T\u00ean \u0111\u0103ng nh\u1eadp ho\u1eb7c m\u1eadt kh\u1ea9u kh\u00f4ng \u0111\u00fang!");
+            url = "/login.jsp";
             
-            // close database connections
-            rs.close();
-            pstmt.close();
-            conn.close();
+        } else if(taiKhoan.getRole() == 1) {
+            final HttpSession session = request.getSession();
+            session.setAttribute("taikhoan", (Object) taiKhoan);
+            url = "/admin-dashboard.jsp";
             
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+        } else if(taiKhoan.getRole() == 2) {
+            final HttpSession session = request.getSession();
+            session.setAttribute("taikhoan", (Object) taiKhoan);
+            url = "/user-dashboard.jsp";
+        final RequestDispatcher rd = this.getServletContext().getRequestDispatcher(url);
+        rd.forward(request, response);
         }
     }
 }
+  
+
+
